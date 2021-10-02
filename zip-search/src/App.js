787b3adf3e69b85a035ad8f67e1ca281b4be7463
Emmanuel Maina <67443566+mbiraemmanuel@users.noreply.github.com>
2021-10-02 10:09:08 -0400

    Added the search by zip code and search by city functionality
    the app allows you  to choose your mode of search by use of radio buttons

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,28 +1,194 @@
 import React, { Component } from 'react';
+import { Card, Radio } from 'react-bootstrap'
 import './App.css';
 
 
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (
+    <div className="cards">
+      <Card>
+        <Card.Header>{props.name}</Card.Header>
+        <Card.Body>
+          <ul>
+            <li>State: {props.state}</li>
+            <li>Location: ({props.lat}, {props.long})</li>
+            <li>Population (estimated): {props.population}</li>
+            <li>Total Wages: {props.wages}</li>
+          </ul>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+function Zip(props){
+  console.log('Code: ' + props.codes);
+  return (
+    <div>
+      <Card>
+        <Card.Header>{props.cityName}</Card.Header>
+        <Card.Body>
+          <div className="zipList">
+            <ul>
+            {props.codes.map((code) => <li className="codes">{code}</li>)}
+            </ul>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  )
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (
+    <div>
+      <label className="zipLabel" htmlFor="zipsearch">Zip Code: &nbsp;</label>
+      <input type="text" onChange={props.changeHandler} id="zipsearch" placeholder="Try 10016"/>
+    </div>
+  );
+}
+
+function CitySearchField(props){
+  return (
+    <div>
+      <label className="cityLabel" htmlFor="citysearch">City: &nbsp;</label>
+      <input type="text" onChange={props.changeHandler} id="citysearch" placeholder="Try Baltimore"/>
+    </div>
+  );
 }
 
 
 class App extends Component {
+
+  state = {
+    zipCode : '',
+    cities : [],
+    zipRequestStatus : '',
+    cityRequestStatus: '',
+    searchBy : '',
+    city : '',
+    zips : [],
+  }
+
+  handleZipSearch = (event) => {
+    console.log('Search text: ' + event.target.value);
+    this.setState({ zipCode : event.target.value})
+    let base = 'http://ctp-zip-api.herokuapp.com/zip/'
+    base = base + event.target.value
+
+    fetch(base)
+    .then((resp) => {
+      if(resp.status === 200){
+        console.log('SUCCESS');
+        this.setState({zipRequestStatus : 'Success'})
+        return resp.json()
+      } else if(resp.status === 404){
+        console.log('NOT FOUND');
+        this.setState({zipRequestStatus : 'Fail'})
+        return resp.json()
+      }
+    })
+    .then((data) => {
+      console.log(data)
+      this.setState({cities : data})
+    })
+    .catch(error => {
+      console.log('Error' + error);
+    })
+
+  }
+  handleSearchType = (event) =>{
+      console.log(event.target.value);
+      var ele = document.getElementsByClassName('searchTypeInput');
+
+      for(let i = 0; i < ele.length; i++){
+        if(ele[i].checked){
+          this.setState({searchBy : ele[i].value})
+        }
+      }
+  }
+  handleCitySearch = (event) => {
+    let city = event.target.value.toUpperCase()
+    console.log('Search City' + city);
+    this.setState({city : city})
+    this.setState({zips : []})
+    let url = `http://ctp-zip-api.herokuapp.com/city/${city}`
+
+    fetch(url)
+    .then((response) => {
+      if(response.status === 200){
+        console.log('SUCCESS');
+        this.setState({cityRequestStatus : 'Success'})
+        return response.json()
+      } else if(response.status === 404){
+        console.log('NOT FOUND');
+        this.setState({cityRequestStatus : 'Fail'})
+        return response.json()
+      }    })
+    .then((data) => {
+      console.log(data);
+      this.setState({zips : data})
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
+        <div className="d-flex justify-content-center">
+          <div className="main">
+              <div className="searchType"> 
+                <div className="inputGroup">
+                  <input type="radio" name="searchtype" id="zipSearch" value="zip" className="searchTypeInput" onChange={this.handleSearchType}/>
+                  <label htmlFor="zipSearch">Search By Zip</label>
+                </div>
+                  
+                <div className="inputGroup">
+                  <input type="radio" name="searchtype" id="citySearch" value="city" className="searchTypeInput" onChange={this.handleSearchType}/>
+                  <label htmlFor="citySearch">Search by city</label>
+                </div>
+              </div>
+            
+            {this.state.searchBy === 'zip' && 
+              <div className="zipSearch">
+
+
+              <ZipSearchField zipCode={this.state.zipCode} changeHandler={this.handleZipSearch}/>
+              <div>
+                {this.state.cities.map((city) => <City name={city.City}
+                                                      state={city.State} 
+                                                      population={city.EstimatedPopulation} 
+                                                      wages={city.TotalWages}
+                                                      lat={city.Lat}
+                                                      long={city.Long}/>)}
+                {this.state.zipRequestStatus === 'Fail' &&
+                  <h6>No Results</h6>
+                }
+              </div>  
+            </div>
+          
+            }
+
+            {this.state.searchBy === 'city' && 
+              <div className="citySearch">
+                  <CitySearchField city={this.state.city} changeHandler={this.handleCitySearch}/>
+                  {/* {this.state.zips.map((zip) => <Zip code/>)} */}
+                  
+                  {this.state.cityRequestStatus === 'Success' && 
+                    <Zip codes={this.state.zips} cityName={this.state.city}/>
+                  }
+                  {this.state.cityRequestStatus === 'Fail' && 
+                    <h6>No Results</h6>
+                  }
+                  
+              </div>
+          
+            }            
+            </div>           
         </div>
+        
       </div>
     );
   }
